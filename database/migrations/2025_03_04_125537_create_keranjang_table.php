214309ee83c55934716a0cfc07ec18ec02ecf796
Adidya Abimanyu <114373205+AdidyaAbimanyu@users.noreply.github.com>
2025-03-04 20:00:30 +0700

    Migrasi (Belum Foreign Key)

---
+++ database/migrations/2025_03_04_125537_create_keranjang_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('keranjang', function (Blueprint $table) {
+            $table->id('ID_Keranjang');
+            $table->string('Nama_Produk');
+            $table->integer('Jumlah_Produk');
+            $table->integer('Harga_Produk');
+            $table->integer('Total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('keranjang');
+    }
+};
